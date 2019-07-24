24cb6b06840ad9b0b6815e1953defa3b1f9a0971
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-07-24 11:40:17 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <form @submit.prevent="add">
+        <input type="hidden" v-model="form.id">
+        <input type="text" v-model="form.name">
+        <button type="submit" v-show="!updateSubmit">add</button>  
+        <button type="button" v-show="updateSubmit" @click="update(form)">Update</button> 
+    </form>
+    <ul v-for="user in users" :key="user.idd">
+      <li>
+      <span>{{user.name}}</span> &#160;
+      <button @click="edit(user)">Edit</button> ||  <button @click="del(user)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */ 
+import axios from 'axios'
+export default {
+  data(){
+    return{
+        form: {
+          id: '',
+          name: ''
+        },
+        users: '',
+        updateSubmit: false
+    }
+  },
+  mounted() {
+    this.load()
+  },
+  methods: {
+    load(){
+        axios.get('http://localhost:3000/users').then(res => {
+        this.users = res.data
+      }).catch ((err) => {
+        console.log(err);
+        
+      })
+    },
+      add(){
+      axios.post('http://localhost:3000/users/', this.form).then(res => {
+          this.load()
+          this.form.name = ''
+      })
+    },
+    edit(user){ 
+        this.updateSubmit = true
+        this.form.id = user.id 
+        this.form.name = user.name 
+    },
+    update(form){ 
+       return axios.put('http://localhost:3000/users/' + form.id , {name: this.form.name}).then(res => {
+        this.load()
+        this.form.id = ''
+        this.form.name = ''
+        this.updateSubmit = false
+      }).catch((err) => {
+        console.log(err);
+        
+      })
+    },
+    del(user){
+      axios.delete('http://localhost:3000/users/' + user.id).then(res =>{
+          this.load()
+          let index = this.users.indexOf(this.form.name)
+          this.users.splice(index,1)
+      })
+    }
+  }
+}
+</script>
